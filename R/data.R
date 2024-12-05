0cdbfb9738e4055f83c3428c18518509b681e395
Tobias Stephan <73840881+tobiste@users.noreply.github.com>
2024-12-04 20:28:12 -0500

    swath vignette added

--- R/data.R
+++ R/data.R
@@ -1,4 +1,4 @@
-#' Sample data set
+#' Exsmple `sf` data set
 #'
 #' example dataset
 #'
@@ -14,3 +14,20 @@
 #' data("locations_example")
 #' head("locations_example")
 "locations_example"
+
+#' Example raster data set
+#'
+#' example dataset
+#'
+#' @docType data
+#'
+#' @usage data('raster_example')
+#'
+#' @format An object of class `matrix`
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data("raster_example")
+#' head("raster_example")
+"raster_example"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,5 +1,6 @@
 ## code to prepare `DATASET` dataset goes here
 
+set.seed(1234)
 locations_example <- readRDS("C:/Users/tstephan/Documents/Lakehead/Goldshore data/Data/goldshore_bh_samples.rds") |>
   dplyr::slice_sample(n = 500)
 locations_example$value <- runif(nrow(locations_example))
@@ -10,3 +11,24 @@ locations_example <- locations_example |>
   dplyr::select(value)
 
 usethis::use_data(locations_example, overwrite = TRUE)
+
+
+library(ncdf4)
+library(dplyr)
+library(ggplot2)
+
+ncpath <- "E:/Global data/ETOPO/"
+ncname1 <- "ETOPO_2022_v1_60s_N90W180_geoid"
+ncname2 <- "ETOPO_2022_v1_60s_N90W180_bed"
+
+ncfname1 <- paste0(ncpath, ncname1, ".nc")
+ncfname2 <- paste0(ncpath, ncname2, ".nc")
+
+geoid <- terra::rast(ncfname1)
+bed <- terra::rast(ncfname2)
+
+wgs84_z <- geoid + bed
+
+raster_example_r <- terra::crop(wgs84_z, terra::vect(locations_example))
+raster_example <- cbind(terra::crds(raster_example_r), terra::values(raster_example_r))
+usethis::use_data(raster_example, overwrite = TRUE)
